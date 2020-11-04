6a2d473ed9f1edf9146832f4cded90770f74a156
LionelBeato <lionel.beato@gmail.com>
2020-11-04 15:03:34 -0500

    first commit

---
+++ src/test/java/works/lionel/seconddeploy/SeconddeployApplicationTests.java
@@ -0,0 +1,13 @@
+package works.lionel.seconddeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeconddeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
